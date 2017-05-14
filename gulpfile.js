245c4483fb78ea909421448075d974be85debbc8
j-r <johnreardon@Johns-MacBook-Air-2.local>
2017-05-13 19:53:15 -0500

    treehouse project 11 v1.2
    Created gulp task to optimize critical CSS. Replaced font files with
    google fonts link.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 
 gulp.task('process-css', () => {
-	return gulp.src('./css/*.css')
+	return gulp.src(['./css/basics.css', './css/footer.css', './css/hero.css', './css/menu.css', './css/modals.css', './css/photo-grid.css'])
 		.pipe(cleanCSS())
 		.pipe(concat('prod.min.css'))
 		.pipe(gulp.dest('./dist'));
